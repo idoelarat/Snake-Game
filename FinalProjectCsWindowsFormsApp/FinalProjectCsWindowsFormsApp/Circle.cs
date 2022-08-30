c9ecaf67c8c449fede72c7040b13936adb4ee0e3
SaharLevy <101057135+SaharLevy@users.noreply.github.com>
2022-08-30 18:47:00 +0300

    Add files via upload

---
+++ FinalProjectCsWindowsFormsApp/FinalProjectCsWindowsFormsApp/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectCsWindowsFormsApp
+{
+    internal class Circle : Figure
+    {
+
+        public Circle()
+        {
+           setXValue(0);
+           setYValue(0);
+        }
+        public Circle(int x, int y)
+        {
+            setXValue(x);
+            setYValue(y);
+        }
+    }
+}

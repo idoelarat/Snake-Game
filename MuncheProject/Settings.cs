1324a6986e1896cd8f50d5c2bf68f64a5279c6c2
Ido <111766182+Idoela@users.noreply.github.com>
2022-09-08 20:05:42 +0300

    Add files via upload

---
+++ MuncheProject/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace MuncheProject
+{
+    class Settings
+    {
+        public static int Width { get; set; } 
+        public static int Height { get; set; }
+        public static string directions;
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            directions = "left";
+        }
+        public double SnakeSize()
+        {
+            return 16 * 1.5;
+        }
+    }
+}
